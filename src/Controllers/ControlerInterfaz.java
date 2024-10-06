bcb45b66309e6d0f4395d4e3b193a2ea0c808471
Luis <lquishpe5771@uta.edu.ec>
2024-10-06 15:57:30 -0500

    Interfaz de editar usuario

--- src/Controllers/ControlerInterfaz.java
+++ src/Controllers/ControlerInterfaz.java
@@ -5,47 +5,79 @@
 package Controllers;
 
 import Models.Estudiante;
+import Views.Interfaz_Editar;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import Views.Interfaz_Principal;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author William
  */
-public class ControlerInterfaz implements ActionListener{
+public class ControlerInterfaz implements ActionListener {
+
     Interfaz_Principal vista = new Interfaz_Principal();
+    Interfaz_Editar editar = new Interfaz_Editar();
     ApiRest api = new ApiRest();
-    public ControlerInterfaz(Interfaz_Principal vista, ApiRest api){
-        this.vista= vista;
+
+    public ControlerInterfaz(Interfaz_Principal vista, ApiRest api, Interfaz_Editar editar) {
+        this.vista = vista;
         this.api = api;
+        this.editar = editar;
         vista.jbtnCrearUsuario.addActionListener(this);
         vista.jbtnEditarUsuario.addActionListener(this);
         vista.jbtnEliminarUsuario.addActionListener(this);
+        editar.jBtnCancelar.addActionListener(this);
+        editar.jBtnGuardar.addActionListener(this);
+
         this.llenarTabla();
     }
-    private void llenarTabla(){
-        ArrayList<Estudiante> estudiantes = this.api.obtenerEstudiantes("http://localhost:8080/SOA/controllers/apiRest.php");
+
+    private void llenarTabla() {
+        ArrayList<Estudiante> estudiantes = this.api.obtenerEstudiantes("http://localhost:8087/SOA/controllers/apiRest.php");
         DefaultTableModel modeloTable = (DefaultTableModel) this.vista.jtblEstudiantes.getModel();
         for (Estudiante estudiante : estudiantes) {
-            modeloTable.addRow(new Object[]{estudiante.getCedula(), estudiante.getNombre(),estudiante.getApellido(),estudiante.getDireccion(),estudiante.getNumeroCelular()});
+            modeloTable.addRow(new Object[]{estudiante.getCedula(), estudiante.getNombre(), estudiante.getApellido(), estudiante.getDireccion(), estudiante.getNumeroCelular()});
         }
         this.vista.jtblEstudiantes.setModel(modeloTable);
     }
-    
-    
+
+    private void editarAlumno(int indice) {
+        ArrayList<Estudiante> estudiantes = this.api.obtenerEstudiantes("http://localhost:8087/SOA/controllers/apiRest.php");
+        String cedula = estudiantes.get(indice).getCedula();
+        this.editar.jLblCedula.setText(cedula);
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource()==vista.jbtnCrearUsuario) {
-            
+    public void actionPerformed(ActionEvent e
+    ) {
+        if (e.getSource() == vista.jbtnCrearUsuario) {
+
         }
-        if (e.getSource()==vista.jbtnEditarUsuario) {
-            
+        if (e.getSource() == vista.jbtnEditarUsuario) {
+            if (this.vista.jtblEstudiantes.getSelectedRow() != -1) {
+                this.editar.setVisible(true);
+                int indice = this.vista.jtblEstudiantes.getSelectedRow();
+                editarAlumno(indice);
+            }else{
+            JOptionPane.showMessageDialog(null, "Seleccione al usuario a editar ");
+            }
+
         }
-        if (e.getSource()==vista.jbtnEliminarUsuario) {
-            
+        if (e.getSource() == vista.jbtnEliminarUsuario) {
+
+        }
+
+        //Editar estudiante
+        if (e.getSource() == editar.jBtnCancelar) {
+            this.editar.dispose();
+        }
+        if (e.getSource() == editar.jBtnGuardar) {
+          
         }
     }
-    
+
 }
